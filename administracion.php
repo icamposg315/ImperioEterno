27adf2a8f42ad35f72ff90ae5709c3f1a18e0c25
icamposg315 <icamposg315@alumnos.imf.com>
2023-04-21 12:46:13 +0200

    ImperioEterno_v3

--- administracion.php
+++ administracion.php
@@ -17,7 +17,7 @@ $resultado4 = $database->getAllClientes();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="administracion.css">
-    <link rel="icon" type="image/x-icon" href="cabeza.ico">
+    <link rel="icon" type="image/x-icon" href="LenguajesDeMarcas/cabeza.ico">
     <title>Administrador</title>
 </head>
 
